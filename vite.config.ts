9bcdb6537718840e0dbe8f887bf0c6f8c54648fa
Yaron, Asaf <asaf_yaron@bmc.com>
2025-05-19 18:43:46 +0300

    IF-123 feat: Add AWS integration support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // ensures assets are loaded from root
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
